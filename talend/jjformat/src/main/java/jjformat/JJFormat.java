661881e313696aeb871cc6eda5cdb047d93385aa
ypiel <ypiel@talend.com>
2017-10-03 08:52:35 +0200

    jjFormat - Generate HTML output of java jet to clearly distinguish java code and generate java code. It count parenthesis too and generate a file with only java all generated code is replaced by sysout.

--- talend/jjformat/src/main/java/jjformat/JJFormat.java
+++ talend/jjformat/src/main/java/jjformat/JJFormat.java
@@ -17,6 +17,8 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 
+import com.google.googlejavaformat.java.Formatter;
+
 import static org.apache.commons.lang3.StringEscapeUtils.escapeHtml4;
 
 public class JJFormat {
@@ -25,6 +27,7 @@ public class JJFormat {
 	private final static String JJ_NORMALIZED = "normalized";
 	private final static String JJ_FORMATTED = "formatted";
 	private final static String JJ_HTML = "html";
+	private final static String JJ_JAVA = "java";
 
 	private final static Map<String, String> exts = new HashMap<>();
 	static {
@@ -32,6 +35,7 @@ public class JJFormat {
 		exts.put(JJ_NORMALIZED, ".javajet");
 		exts.put(JJ_FORMATTED, ".javajet");
 		exts.put(JJ_HTML, ".html");
+		exts.put(JJ_JAVA, ".java");
 	}
 
 	Map<String, String> jj_versions = new HashMap<String, String>();
@@ -40,6 +44,7 @@ public class JJFormat {
 	private File jj_file;
 	private File out_dir;
 	private String name_output = "";
+	private boolean debug = false;
 
 	public static void main(String[] args) {
 		JJFormat jjf = new JJFormat();
@@ -56,7 +61,34 @@ public class JJFormat {
 		this.normalize();
 		this.format();
 		this.transform2HTML();
+		this.onlyJava();
 		this.writeVersions();
+	}
+
+	private void onlyJava() {
+		String content = this.jj_versions.get(JJ_NORMALIZED);
+
+		content = content.replaceAll("<%=[ a-zA-Z-_().]*?%>", "");
+		content = content.replaceAll("@", "\n//@");
+		content = content.replaceAll("%>.*?<%", "\nSystem.out.println(\"Java jet code ...\");\n");
+		content = content.replaceAll(";", ";\n");
+
+		String begin = content.substring(0, 2);
+		if("<%".equals(begin)){
+			content = content.substring(2, content.length());
+		}
+		
+		content = content.replaceAll("%>.*", ""); // remove last javajet code
+		content = "public class Gen {\npublic static void main(String[] args) {\n" + content + "\n}\n}";
+		try {
+			content = new Formatter().formatSource(content);
+		} catch (Exception e) {
+			System.out.println("Can't formated java output : " + e.getMessage());
+			System.out.println("Use personal formatter...");
+			e.printStackTrace();
+		}
+
+		this.jj_versions.put(JJ_JAVA, content);
 	}
 
 	private void transform2HTML() {
@@ -67,9 +99,9 @@ public class JJFormat {
 		content = "<div><pre>\n" + content;
 
 		content = content.replaceAll("\n&lt;%",
-				"\n</pre></div>\n<div style=\"background-color: #ffffe6;\"><pre>\n&lt;%");
+				"\n</pre></div>\n<div style=\"background-color: #ffffe6;\"><pre style=\"margin: 0px\">\n&lt;%");
 		content = content.replaceAll("\n%&gt;",
-				"\n%&gt;\n</pre></div>\n<div style=\"background-color: #e6ffff;\"><pre>\n");
+				"\n%&gt;\n</pre></div>\n<div style=\"background-color: #e6ffff;\"><pre style=\"margin: 0px\">\n");
 
 		content = "<html><body>" + content + "</body></html>";
 
@@ -78,11 +110,21 @@ public class JJFormat {
 
 	private void format() {
 		String content = this.jj_versions.get(JJ_NORMALIZED);
+		content = formatJava(content, this.debug);
+		this.jj_versions.put(JJ_FORMATTED, content);
+	}
 
+	private String formatJava(String content, boolean debug) {
+		String java_parenthesis_flow = "";
+
+		// String content = this.jj_versions.get(JJ_NORMALIZED);
+
+		content = content.replaceAll("\"@\\{[a-zA-Z.-_]+\\}", "\"@xxxxxx");
 		content = content.replaceAll("<%([^=])", "\n<%\n$1");
 		content = content.replaceAll("%>", "\n%>\n");
 		content = content.replaceAll("<%=(.*)\n", "<%=$1");
 		content = content.replaceAll("<%=(.*)%>\n", "<%=$1%>");
+		content = content.replaceAll("for[ ]*\\(([^;\n]+);([^;\n]+);([^)\n]+)\\)", "for($1| $2| $3)");
 		content = content.replaceAll(";", ";\n");
 		content = content.replaceAll("\\{", "\\{\n");
 		content = content.replaceAll("\\}", "\\}\n");
@@ -113,6 +155,8 @@ public class JJFormat {
 			int tab_java = 0;
 			int tab_jj = 0;
 			newContent = "";
+			String java_line = "";
+			boolean is_java_parenthesis_line = false;
 			for (int numcar = 0; numcar < cc.length; numcar++) {
 				char c = cc[numcar];
 				char next = 'X';
@@ -131,18 +175,18 @@ public class JJFormat {
 				if (c == '{') {
 					if (is_java) {
 						tab_java++;
+						is_java_parenthesis_line = true;
 					} else {
 						tab_jj++;
 					}
 				}
 
-				/*
-				 * if (c == '}') { if (is_java) { tab_java--; } else { tab_jj--;
-				 * } if (tab_java < 0 || tab_jj < 0) { throw new
-				 * Exception("Nb tab can't be negative"); } }
-				 */
+				if (is_java && c == '}') {
+					is_java_parenthesis_line = true;
+				}
 
 				newContent += c;
+				java_line += c;
 
 				if (c == '\n') {
 
@@ -158,13 +202,27 @@ public class JJFormat {
 					}
 
 					int nb = is_java ? tab_java : tab_jj;
+
+					if (is_java_parenthesis_line) {
+						java_parenthesis_flow += java_line;
+						is_java_parenthesis_line = false;
+					}
+					java_line = "";
+
 					for (int i = 0; i < nb; i++) {
 						newContent += "    ";
+
+						if (is_java_parenthesis_line) {
+							java_parenthesis_flow += "    ";
+						}
 					}
 				}
 
 				prev = c;
 			}
+
+			System.out.println("Nb Java open parathesis not closed : " + tab_java);
+			System.out.println("Nb JavaJet open parathesis not closed : " + tab_jj);
 		} catch (Exception e) {
 			System.out.println("Can't format : " + e.getMessage());
 			e.printStackTrace();
@@ -173,7 +231,23 @@ public class JJFormat {
 		newContent = newContent.replaceAll(" *<% *", "<%");
 		newContent = newContent.replaceAll(" *%> *", "%>");
 
-		this.jj_versions.put(JJ_FORMATTED, newContent);
+		if (debug) {
+			// java_parenthesis_flow = "public class GenDebugParenthesis {\n
+			// public static void main(String[] args) {\n" +
+			// java_parenthesis_flow+"\n}\n}\n}";
+			/*
+			 * try{ java_parenthesis_flow = new
+			 * Formatter().formatSource(java_parenthesis_flow); }
+			 * catch(Exception e){
+			 * System.out.println("Can't formated java parenthesis class : " +
+			 * e.getMessage()); e.printStackTrace(); }
+			 */
+
+			this.writeAFile("java_parenthesis", java_parenthesis_flow, ".java");
+		}
+
+		// this.jj_versions.put(JJ_FORMATTED, newContent);
+		return newContent;
 	}
 
 	private void normalize() {
@@ -189,8 +263,12 @@ public class JJFormat {
 	}
 
 	private void writeAVersion(String file, String content) {
+		this.writeAFile(file, content, exts.get(file));
+	}
+
+	private void writeAFile(String file, String content, String ext) {
 		try {
-			File out = new File(this.out_dir, name_output + file + exts.get(file));
+			File out = new File(this.out_dir, name_output + file + ext);
 			PrintWriter pw = new PrintWriter(out);
 			pw.write(content);
 			pw.close();
@@ -213,6 +291,7 @@ public class JJFormat {
 				if (!first) {
 					content += "\n";
 				}
+				l = l.replaceFirst("//.*", "");
 				content += l;
 				first = false;
 			}
@@ -236,6 +315,10 @@ public class JJFormat {
 		Option oName = new Option("name", false, "Name output with component name");
 		oName.setRequired(false);
 		options.addOption(oName);
+
+		Option gen_debug = new Option("debug", false, "Generate debug files");
+		gen_debug.setRequired(false);
+		options.addOption(gen_debug);
 	}
 
 	public void configure(String[] args) {
@@ -260,7 +343,11 @@ public class JJFormat {
 			}
 
 			if (cl.hasOption("name")) {
-				this.name_output = jj_file.getName().split("_")[0] + "_";
+				this.name_output = jj_file.getName().split("\\.")[0] + "_";
+			}
+
+			if (cl.hasOption("debug")) {
+				this.debug = true;
 			}
 
 		} catch (Exception e) {
